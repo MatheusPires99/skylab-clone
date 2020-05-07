3166f5cb015543c987e9713d04e390f55e47a0f3
Matheus Pires <matheushenriquepires99@gmail.com>
2020-05-07 20:29:43 -0300

    Using my-json-server URL on axios

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "https://my-json-server.typicode.com/MatheusPires99/skylab-clone",
 });
 
 export default api;
